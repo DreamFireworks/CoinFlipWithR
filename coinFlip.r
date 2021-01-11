1f1425c1cfcee37075e5c2de629563ffd8568f78
Serhan Eraslan <42625066+DreamFireworks@users.noreply.github.com>
2021-01-11 09:57:27 +0300

    added comments

--- coinFlip.r
+++ coinFlip.r
@@ -1,10 +1,12 @@
 # Github: DreamFireworks
 # Serhan Eraslan
-size<-c(10000) #Deney buyuklugu.
-flip<-c(sample((0:1),size= size, replace=T)) #Rastgele atis
-tails<-sum(flip)/size #Yazi gelme ihtimali
-heads<-1-yazi #Tura gelme ihtimali
+size<-c(10000) # Simulation size
+flip<-c(sample((0:1),size= size, replace=T)) # Random flipping
+tails<-sum(flip)/size # Prob. of tails
+heads<-1-yazi # Prob. of heads
+
 result<-data.frame(size,tails,heads)
-colnames(result)<-c("DENEY BUYUKLUGU","YAZI ORANI","TURA ORANI")
-rownames(result)<-c("1.DENEY")
-sonuc
+colnames(result)<-c("SIM. SIZE","PROB. OF TAILS","PROB. OF HEADS")
+rownames(result)<-c("SIM. 1")
+
+result
